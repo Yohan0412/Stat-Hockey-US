65979e90319acb81dbab9472d746787108a005b4
Yohan0412 <Yohan.rosano@hotmail.fr>
2024-03-15 16:37:42 +0100

    trinita

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, { useState } from 'react';
+import {  useEffect } from "react";
+import instance from './service/instance';
 
 function App() {
+
+   const [joueurswash , setJoueurswash] = useState("");
+
+
+  const getEmployee = () => {
+    // Send the request
+     fetch('http://localhost:4000' ,  {method: 'GET',
+     headers: {
+       'Accept': 'application/json',
+       'Content-Type': 'application/json',       
+     }})
+      // Use this data to update the state
+      .then((response) => response.json() )
+      .then((responseJson) => {
+        console.log(responseJson);
+        // setJoueurswash(response.data.skaters);
+      });
+  };
+
+
+  const [joueurs, setJoueurs] = useState([]);
+
+  useEffect(() => {
+    instance
+      .get(`/skatersWASHINGTON`)
+      .then((result) => {
+        console.log(result);
+        setJoueurs(result);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>NHL</h1>
+       <button type="button" onClick={getEmployee}>Get Data</button>
+       
       </header>
     </div>
   );
